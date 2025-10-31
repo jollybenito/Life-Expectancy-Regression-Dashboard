a3280cc1bcd39a5f7cb285cfe7818121772769fa
jollybenito <59927578+jollybenito@users.noreply.github.com>
2025-10-30 23:10:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -645,4 +645,4 @@ def feature_importances(data):
 server = app.server
 
 if __name__ == "__main__":
-    app.run_server(host="0.0.0.0", port=8050, debug=True)
+    app.run_server(host="0.0.0.0", port=10000, debug=True)
